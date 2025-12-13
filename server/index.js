973b49dc39218ca81946ef64fa6c888afdee8f0d
Intemic <admin@intemic.com>
2025-12-13 01:00:37 +0100

    lil fix for openai

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { initDb } = require('./db');
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const cookieParser = require('cookie-parser');
 const { register, login, logout, authenticateToken, getMe, getOrganizations, switchOrganization, getOrganizationUsers, inviteUser } = require('./auth');
 
